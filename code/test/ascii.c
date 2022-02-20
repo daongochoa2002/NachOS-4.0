ce69276075e23937a349bbad39458d2076deba21
Đào Ngọc Hoa <74868806+daongochoa2002@users.noreply.github.com>
2022-02-20 08:36:20 +0700

    Update ascii.c

--- code/test/ascii.c
+++ code/test/ascii.c
@@ -6,13 +6,13 @@ main()
     //In ra bang ma ascii
     int i,j;
     PrintString("\t\tAscii Table\n");
-    for(i = 32; i <= 50; i++)
+    for(i = 33; i <= 51; i++)
     {
         for(j = 0; j < 5; j++)
         {
             PrintNum(i + j * 19);
             PrintString(" - ");
-            PrintChar(i + j);
+            PrintChar(i + j*19) ;
             PrintString("\t");
         }
         PrintString("\n");
